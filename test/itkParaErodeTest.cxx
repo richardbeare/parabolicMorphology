ca368c2656f6084ba3418e91b1ac9ebb30a00d2e
Richard Beare <richard.beare@monash.edu>
2015-04-06 20:53:50 +1000

    erode test

--- test/itkParaErodeTest.cxx
+++ test/itkParaErodeTest.cxx
@@ -11,7 +11,7 @@
 
 // sanity check of the image spacing option 
 
-int main(int argc, char * argv[])
+int itkParaErodeTest(int argc, char * argv[])
 {
   itk::MultiThreader::SetGlobalMaximumNumberOfThreads(1);
   const int dim = 2;
@@ -28,7 +28,15 @@ int main(int argc, char * argv[])
   typedef itk::ImageFileReader< IType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
-  reader->Update();
+  try 
+    {
+      reader->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+      std::cerr << excp << std::endl;
+      return EXIT_FAILURE;
+    }
 
   typedef itk::ParabolicErodeImageFilter< IType,IType > FilterType;
 
@@ -39,7 +47,15 @@ int main(int argc, char * argv[])
   filter->SetScale(scale);
   filter->SetUseImageSpacing(true);
   filter->SetParabolicAlgorithm(FilterType::INTERSECTION);
-  filter->Update();
+  try 
+    {
+      filter->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+      std::cerr << excp << std::endl;
+      return EXIT_FAILURE;
+    }
 
   typedef itk::ImageFileWriter< IType > WriterType;
   WriterType::Pointer writer = WriterType::New();
@@ -54,8 +70,16 @@ int main(int argc, char * argv[])
 
   writer->SetInput( filter->GetOutput() );
   writer->SetFileName( argv[3] );
-  writer->Update();
-   
+  try
+    {
+      writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+      std::cerr << excp << std::endl;
+      return EXIT_FAILURE;
+    }
+
   return EXIT_SUCCESS;
 }
 
