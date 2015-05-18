4896450e03efe798c79b8919d84678668e15a999
Richard Beare <richard.beare@monash.edu>
2015-05-18 21:18:03 +1000

    KW and uncrust

--- oldtests/check.cxx
+++ oldtests/check.cxx
@@ -9,18 +9,17 @@
 #include "itkTimeProbe.h"
 #include "itkMultiThreader.h"
 
-
-int main(int, char * argv[])
+int main(int, char *argv[])
 {
   //itk::MultiThreader::SetGlobalMaximumNumberOfThreads(1);
   const int dim = 2;
 
-  typedef unsigned char PType;
+  typedef unsigned char            PType;
   typedef itk::Image< PType, dim > IType;
 
   typedef itk::ImageFileReader< IType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName( argv[1] );
+  reader->SetFileName(argv[1]);
 
   typedef itk::ParabolicErodeImageFilter< IType, IType > FilterType;
 
@@ -29,14 +28,14 @@ int main(int, char * argv[])
   filter->SetInput( reader->GetOutput() );
 
   FilterType::RadiusType scale;
-  scale[0]=1;
-  scale[1]=0.5;
+  scale[0] = 1;
+  scale[1] = 0.5;
 
 //  filter->SetParabolicAlgorithm(FilterType::CONTACTPOINT);
   filter->SetScale(scale);
   itk::TimeProbe NewTime;
   filter->SetUseImageSpacing(true);
-  for (unsigned i = 0; i < 100; i++)
+  for ( unsigned i = 0; i < 100; i++ )
     {
     filter->Modified();
     NewTime.Start();
@@ -47,11 +46,10 @@ int main(int, char * argv[])
   typedef itk::ImageFileWriter< IType > WriterType;
   WriterType::Pointer writer = WriterType::New();
   writer->SetInput( filter->GetOutput() );
-  writer->SetFileName( argv[2] );
+  writer->SetFileName(argv[2]);
   writer->Update();
   std::cout << std::setprecision(3)
             << NewTime.GetMean() << std::endl;
 
   return 0;
 }
-
