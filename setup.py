92899e01adfa9a6c29d6ac8219b027c74298c6ce
Mathew Seng <mathewseng@gmail.com>
2021-01-11 15:19:07 -0600

    COMP: Update GitHub Actions from ITKModuleTemplate

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     keywords='ITK Parabolic Morphology Erosion Dilation',
     url=r'https://github.com/InsightSoftwareConsortium/ITKParabolicMorphology',
     install_requires=[
-        r'itk>=5.1.0.post2'
+        r'itk>=5.2rc1'
     ]
     )
